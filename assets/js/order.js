bfc4ba2247d78195a0bacbe0259dc4faa4386604
hridesh <hridesh699@gmail.com>
2024-05-09 19:29:25 +0530

    completed orders page

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,24 +1,45 @@
-const dispatched_btn = document.getElementById("dispatched");
+const processed_btn = document.getElementById("processed");
 const en_route_btn = document.getElementById("en_route");
 const delivered_btn = document.getElementById("delivered");
 
 const defaultColor = "#0F4C75";
 const activecolor = "#172025";
 
-dispatched_btn.addEventListener("click", () => {
-  dispatched_btn.style.backgroundColor = activecolor;
+var orders = document.querySelectorAll(".order-delivered, .order-en-route, .order-processed");
+
+function filter(status) {
+  orders.forEach((element) => {
+    if (!element.classList.contains(status)) {
+      element.style.display = "none";
+    } else {
+      element.style.display = "";
+    }
+  });
+}
+
+function clear_filter() {
+  orders.forEach((element) => {
+    element.style.display = "";
+  });
+}
+
+processed_btn.addEventListener("click", () => {
+  filter("order-processed");
+  processed_btn.style.backgroundColor = activecolor;
   en_route_btn.style.backgroundColor = defaultColor;
   delivered_btn.style.backgroundColor = defaultColor;
 });
 
 en_route_btn.addEventListener("click", () => {
-  dispatched_btn.style.backgroundColor = defaultColor;
+  filter("order-en-route");
+  processed_btn.style.backgroundColor = defaultColor;
   en_route_btn.style.backgroundColor = activecolor;
   delivered_btn.style.backgroundColor = defaultColor;
 });
 
 delivered_btn.addEventListener("click", () => {
-  dispatched_btn.style.backgroundColor = defaultColor;
+  filter("order-delivered");
+  processed_btn.style.backgroundColor = defaultColor;
   en_route_btn.style.backgroundColor = defaultColor;
   delivered_btn.style.backgroundColor = activecolor;
 });
